3f19c08ccda76cd2759afecfca98c8a72289852d
osmar <osmarrod18@gmail.com>
2024-05-16 17:39:24 +0100

    updated eloquent models

---
+++ app/Models/Chat.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Chat extends Model
+{
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'seen_at' => 'datetime',
+        'message_deleted_at' => 'datetime',
+    ];
+
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id', 'id');
+    }
+
+    public function reply(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Chat::class, 'reply_id', 'id');
+    }
+
+    public function receiver(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'receiver_id', 'id');
+    }
+
+    public function sender(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id', 'id');
+    }
+}
