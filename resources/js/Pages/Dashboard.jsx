0d6dac175e3729d6e230849ab4117bb668f6a02d
osmar <osmarrod18@gmail.com>
2024-05-17 18:21:30 +0100

    added chat button

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import {Head, Link} from '@inertiajs/react';
+import React from "react";
 
 export default function Dashboard({ auth }) {
     return (
@@ -13,6 +14,20 @@ export default function Dashboard({ auth }) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900 dark:text-gray-100">You're logged in!</div>
+
+                    </div>
+                </div>
+
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 flex justify-center items-center my-10">
+                    <div className="bg-white mx-auto  dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <Link
+                            as="button"
+                            method="get"
+                            href={route('chat.index')}
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        >
+                            Go To Chats
+                        </Link>
                     </div>
                 </div>
             </div>
